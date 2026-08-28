de4214b17b04c0755d0bc7497b8ff5116ee15b4d
Alturist <venture.solutions3.com>
2026-08-28 13:16:37 +0000

    fix: force-push pre-compiled local tina client schemas to vercel

---
+++ tina/__generated__/config.prebuild.jsx
@@ -0,0 +1,46 @@
+// tina/config.js
+import { defineConfig } from "tinacms";
+var config_default = defineConfig({
+  branch: "main",
+  clientId: null,
+  token: null,
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public"
+  },
+  media: {
+    tina: {
+      mediaRoot: "uploads",
+      publicFolder: "public"
+    }
+  },
+  // Force local production flags
+  isCloud: false,
+  local: true,
+  schema: {
+    collections: [
+      {
+        name: "product",
+        label: "Corporate Products",
+        path: "src/content/products",
+        format: "md",
+        fields: [
+          { type: "string", name: "title", label: "Product Name", isTitle: true, required: true },
+          {
+            type: "string",
+            name: "category",
+            label: "Category",
+            options: ["Tech & Lifestyle", "Office Stationery", "Hampers", "Apparel", "Eco-Friendly", "Leather Goods", "Custom Decor"]
+          },
+          { type: "image", name: "image", label: "Display Image" },
+          { type: "number", name: "price", label: "Base Price (\u20B9)" },
+          { type: "number", name: "moq", label: "Minimum Order Quantity (MOQ)" },
+          { type: "rich-text", name: "body", label: "Product Description", isBody: true }
+        ]
+      }
+    ]
+  }
+});
+export {
+  config_default as default
+};
